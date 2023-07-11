70c5eb5eff1438bb4f133c12c12c7024c4e7c427
anushkabirla19 <anushkaabirla794@gmil.com>
2023-07-11 20:23:49 +0530

    Updates

---
+++ precache-manifest.ed5eafe7e1e75efcc15bbd0325334161.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ec7e36622a2358a08932969ea27585",
+    "url": "/notes-keeper-website/index.html"
+  },
+  {
+    "revision": "bd78506cf17db0d09f38",
+    "url": "/notes-keeper-website/static/js/2.0392c870.chunk.js"
+  },
+  {
+    "revision": "1b9e60a582e030adfc54",
+    "url": "/notes-keeper-website/static/js/main.396e223b.chunk.js"
+  },
+  {
+    "revision": "4ea4636e41600e9a9219",
+    "url": "/notes-keeper-website/static/js/runtime-main.30e8d252.js"
+  }
+]);
